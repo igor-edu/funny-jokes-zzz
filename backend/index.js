086e2adb8dfbe72a8b0aa8da7b4808e63da82748
igor <igycol@yahoo.co.uk>
2021-05-16 16:07:00 +0200

    updated package.json

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
+
 import jokesRoutes from './routes/jokesRoutes.js';
 import usersRoutes from './routes/usersRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
@@ -18,17 +20,35 @@ app.use(cors());
 //   next();
 // });
 
-app.get('/', (req, res) => {
-  res.send('Hello from index.js');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello from index.js');
+// });
 
 app.use('/jokes', jokesRoutes);
 app.use('/users', usersRoutes);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running... in development mode...');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT;
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 5000;
+}
+
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
